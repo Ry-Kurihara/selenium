3f03ec3deadaa3f1df303003193f0452afb8a7fd
Ry_Kurihara <aeta2308@chiba-u.jp>
2020-12-28 01:49:56 +0900

    大枠完成

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, abort
 import os
+import pandas as pd 
+from sqlalchemy import create_engine, engine
+
+from lib import print_test
+from lib import selen_autopurchase
+import boto3 
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -9,28 +15,33 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, QuickReplyButton, QuickReply, TextSendMessage, TemplateSendMessage, ImageSendMessage, PostbackEvent,
+    CarouselColumn, CarouselTemplate, ButtonsTemplate,
+    PostbackAction, URIAction, MessageAction, messages, 
+
 )
+from linebot.models.template import CarouselColumn, TemplateSendMessage
+from sqlalchemy.sql.elements import Null
 
 app = Flask(__name__)
 
-#環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+HEROKU_POSTGRES_URL = os.environ["HEROKU_POSTGRES_URL"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 @app.route("/")
 def hello_world():
-    return "hello world!"
+    return "LINE API HOME!"
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # get X-Line-Signature header value: signatureは意味不明な文字列
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
+    # get request body as text: bodyはjson形式
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
@@ -46,9 +57,169 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=event.message.text)
+    )
+
+# クイックリプライ実践：下に書いたほうに上書きされる
+@handler.add(MessageEvent, message=TextMessage)
+def response_massage(event):
+    language_list = ["Ruby", "Python", "PHP"]
+    items = [QuickReplyButton(action=MessageAction(label=f"{langage}", text=f"{langage}だ")) for langage in language_list]
+    messages = TextSendMessage(text="なんの言語？", quick_reply=QuickReply(items=items))
+    print("クイックリプライ作動")
+    line_bot_api.reply_message(
+        event.reply_token,
+        messages = messages 
+    )
+
+# カルーセルテンプレート実践
+@handler.add(MessageEvent, message=TextMessage)
+def response_carousel(event):
+    carousel_template_message = TemplateSendMessage(
+        alt_text="not_data",
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url='https://example.com/item1.jpg',
+                    title='this_is',
+                    text='description1',
+                    actions=[
+                        PostbackAction(
+                        label='postback1',
+                        display_text='postback text1',
+                        data='action=buy&itemid=1'
+                        ),
+                        MessageAction(
+                            label='message1',
+                            text='message text1'
+                        ),
+                        URIAction(
+                            label='uri1',
+                            uri='http://example.com/1'
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://example.com/item2.jpg',
+                    title='this is menu2',
+                    text='description2',
+                    actions=[
+                        PostbackAction(
+                            label='postback2',
+                            display_text='postback text2',
+                            data='action=buy&itemid=2'
+                        ),
+                        MessageAction(
+                            label='message2',
+                            text='message text2'
+                        ),
+                        URIAction(
+                            label='uri2',
+                            uri='http://example.com/2'
+                        )
+                    ]
+                )
+            ]
+        )
+    )
+    line_bot_api.reply_message(
+        event.reply_token,
+        messages=carousel_template_message
+    )
+
+# URLのキャッチ、監視時間の設定を聞く
+@handler.add(MessageEvent, message=TextMessage)
+def get_url_and_ask_time(event):
+    engine = create_engine(HEROKU_POSTGRES_URL)
+
+    user_id = str(event.source.user_id)
+    timestamp = str(event.timestamp)
+    message = event.message.text 
+
+    df = pd.DataFrame(data=[[user_id, timestamp, message]], columns=['user_id', 'timestamp', 'message'])
+    df.to_sql('line_autopurchase', con=engine, if_exists='append', index=False)
+
+    if event.message.text in ('Amazonで購入'):
+        messages = TextSendMessage(text='URLを入力してください')
+        line_bot_api.reply_message(
+            event.reply_token,
+            messages=messages
+        )
+
+    elif 'asin_is:' in event.message.text:
+        asin = event.message.text[8:] # url_isはいらない
+        image_url = f'https://images-na.ssl-images-amazon.com/images/P/{asin}.09.THUMBZZZ.jpg'
+        buttons_message = TemplateSendMessage(
+            alt_text='画像が表示できません',
+            template=ButtonsTemplate(
+                thumbnail_image_url=image_url,
+                title='Is_it?',
+                text='we purchase this. OKEEEEEEY??',
+                actions=[ 
+                    PostbackAction(
+                        label='購入します',
+                        # display_text='購入', ユーザーが送るメッセージはいらない
+                        data='itempurchase'
+                    ),
+                    MessageAction(
+                        label='買わない',
+                        text='買わないよ'
+                    ),
+                ]
+            )
+        )
+        line_bot_api.reply_message(
+            event.reply_token,
+            messages=buttons_message
+        )
+
+    elif event.message.text in ('買わないよ'):
+        messages = TextSendMessage(text='そうですか')
+        line_bot_api.reply_message(
+            event.reply_token,
+            messages=messages
+        )
+
+    else:
+        messages = TextSendMessage(text='何を言っているのかわかりません')
+        line_bot_api.reply_message(
+            event.reply_token,
+            messages=messages
+        )
+
+@handler.add(PostbackEvent)
+def get_target_item(event):
+    timestamp = str(event.timestamp)
+    options_with_env = selen_autopurchase.PurchaseClass()
+    options_with_env.get_item(timestamp=timestamp)
+
+    image_name = f'shot{timestamp}.png'
+    s3_client = boto3.client('s3')
+    s3_image_url = s3_client.generate_presigned_url(
+        ClientMethod = 'get_object',
+        Params = {'Bucket': 'my-bucket-ps5', 'Key': image_name},
+        ExpiresIn = 30,
+        HttpMethod = 'GET'
+    )
+    print(f'URLは{s3_image_url}だあああああああああ')
+
+    message = TextSendMessage(text="購入確認します！")
+    image_message = ImageSendMessage(
+            original_content_url=s3_image_url,
+            preview_image_url=s3_image_url, #'https://my-bucket-ps5.s3-ap-northeast-1.amazonaws.com/proto3.png'
+        )
+    messages = [message, image_message]
+    line_bot_api.reply_message(
+        event.reply_token,
+        messages=messages
+    )
+
+
+
 
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    # debug環境（wsgirefサーバ）で動作させるときはこちらを使う
+    app.run()
+    # herokuでgunicornを使うときはこっち
+    # port = int(os.getenv("PORT", 5000))
+    # app.run(host="0.0.0.0", port=port)
---
+++ lib/selen_autopurchase.py
@@ -0,0 +1,118 @@
+import os
+import logging 
+# import cv2
+# buildpack apkを消す。（https://github.com/heroku/heroku-buildpack-apt）opencv-python消す：appfile消す
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+import boto3 
+
+logger = logging.getLogger(__name__)
+
+class PurchaseClass:
+    def __init__(self):
+        # Seleniumをあらゆる環境で起動させるChromeオプション
+        self.options = Options()
+        self.options.add_argument('--disable-gpu');
+        self.options.add_argument('--disable-extensions');
+        self.options.add_argument('--proxy-server="direct://"');
+        self.options.add_argument('--proxy-bypass-list=*');
+        self.options.add_argument('--start-maximized');
+
+        # ※herokuなどの本番環境でヘッドレスモードを使用する
+        env = os.environ['APP_ENV']
+        if env == 'mywin':
+            self.options.add_argument('--headless'); # たまにヘッドレスモードで確認したい
+            pass
+        else:
+            self.options.add_argument('--headless');
+
+        if env == 'mywin':
+            self.DRIVER_PATH = '/Users/ryzerk/develop/python/selenium/chromedriver'
+        elif env == 'heroku':
+            self.DRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+        else:
+            logger.warning('環境を設定してください！')
+            raise Exception
+
+        
+
+    def get_item(self, timestamp):
+        # ブラウザの起動
+        print('起動します')
+        driver = webdriver.Chrome(executable_path=self.DRIVER_PATH, chrome_options=self.options)
+
+        ##ウィンドウサイズの指定
+        driver.set_window_size(1920, 1080)
+
+        # Googleにアクセスする
+        url = 'https://google.com/'
+        driver.get(url)
+
+        # Amazonにアクセスする
+        url = 'https://www.amazon.co.jp/dp/B08GG247WR/ref=s9_acss_bw_cg_toio_md1_w?&me=AN1VRQENFRJN5&pf_rd_m=A3P5ROKL5A1OLE&pf_rd_s=merchandised-search-4&pf_rd_r=W83F5KPFR335M79YGQ4X&pf_rd_t=101&pf_rd_p=6cc9fda7-b07a-4770-bec3-ee1dff21047b&pf_rd_i=3355676051'
+        driver.get(url)
+
+        selector = '#availability'
+        element = driver.find_element_by_css_selector(selector)
+        availability = element.text
+
+        # 在庫あり、入荷予定、在庫切れの3種類っぽい
+        if '在庫あり' in availability:
+            logger.warning('在庫あったーーーーーー')
+            pass
+        elif '入荷予定' in availability:
+            logger.warning('入荷予定らしい')
+        elif '在庫切れ' in availability:
+            logger.warning('在庫切れらしい')
+        else:
+            logger.warning(f'何にも当てはまってないお：availability is {availability} ですよ')
+            
+        #In[]:
+
+        #再読み込み
+        # driver.refresh()
+
+        #現在のURL取得
+        # driver.current_url 
+
+        #スクリーンショットの保存
+        ##ウィンドウサイズの指定
+        # driver.set_window_size(1250, 1036)
+        #スクリーンショットを撮る
+        image_name = f'shot{timestamp}.png'
+        driver.save_screenshot(image_name)
+        s3_resorce = boto3.resource('s3')
+        s3_resorce.Bucket('my-bucket-ps5').upload_file(image_name, image_name)
+        # imgCV = cv2.imread('proto.png')
+        # cv2.imshow("image", imgCV)
+        # cv2.waitKey(0)
+
+        # elem_login_btn = driver.find_element_by_id('add-to-cart-button')
+        # elem_login_btn.click()
+
+        # driver.get('https://amazon.co.jp/gp/cart/view.html/ref=nav_cart')
+
+        # login = driver.find_element_by_name('proceedToRetailCheckout')
+        # login.click()
+
+        # # すでにログイン済みの場合は注文内容確認ページへ遷移する
+        # # ### ログイン済みの場合エラーになってしまうので条件分岐が必要
+
+        # driver.find_element_by_id('ap_email').send_keys(os.environ['AMAZON_EMAIL'])
+
+        # driver.find_element_by_id('continue').click()
+
+        # driver.find_element_by_id('ap_password').send_keys(os.environ['AMAZON_PASS'])
+
+        # driver.find_element_by_id('signInSubmit').click()
+
+        driver.quit()
